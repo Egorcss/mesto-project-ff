df1e65d2cafd5b4f6dd5487a2d04f4481c7d1c6a
Egor Doronin <egor197rus@yandex.ru>
2024-09-09 14:25:10 +0300

    Исправил замечания 1 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,37 @@
 // @todo: Темплейт карточки
 
-const CardTemplate = document.querySelector('#card-template').content;
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
 
 // Сюда будут добавляться карточки
-const PlacesList = document.querySelector('.places__list');
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
-function AddCardOnPage(Card, DeleteCard) {
+function createCard(Card, DeleteCard) {
     // В CardTemplate определен контент темлейта, теперь клонируем его содержимое 
-    const NewCard = CardTemplate.querySelector('.places__item').cloneNode(true);
+    const newCard = cardTemplate.querySelector('.places__item').cloneNode(true);
 
     // В 1 аргументе Card будет элемент массива initialCards и через этот аргумент работаем 
     // со свойствами(link и name)
-    NewCard.querySelector('.card__image').src = Card.link;
-    NewCard.querySelector('.card__title').textContent = Card.name;
+    newCard.querySelector('.card__image').src = Card.link;
+    newCard.querySelector('.card__title').textContent = Card.name;
     
-    // кнопка "мусор", по клику передается аргумент DeleteCard ей, переданный в функцию ранее
-    const ButtonDelete = NewCard.querySelector('.card__delete-button');
-    ButtonDelete.addEventListener('click', () => {
-        DeleteCard();
+    // кнопка "корзинка", по клику передается аргумент DeleteCard ей, переданный в функцию ранее
+    const buttonDelete = newCard.querySelector('.card__delete-button');
+    // Исправил ошибку, надо было добавить NewCard в DeleteCard()
+    buttonDelete.addEventListener('click', () => {
+        DeleteCard(newCard);
     });
 
-    return NewCard;
+    return newCard;
 }
 
 // @todo: Функция удаления карточки
 
-function RemoveCardOnPage() {
-    // выбрал целый элемент li 
-    const CardRemove = document.querySelector('.places__item'); 
-    CardRemove.remove();
+function removeCardOnPage(cardElement) {
+    cardElement.remove();
 };
 
 // @todo: Вывести карточки на страницу
@@ -40,8 +39,8 @@ function RemoveCardOnPage() {
 initialCards.forEach(function (elem) {
     // Из массива initialCards передаю в качестве 1 аргумента каждый элемент массива(elem), 
     // а в качестве 2 аругмента - функцию удаления, но она сработает только по клику
-    // на кнопку "мусор"
-    const CardElement = AddCardOnPage(elem, RemoveCardOnPage);
-    PlacesList.append(CardElement);
+    // на кнопку "корзинка"
+    const cardElement = createCard(elem, removeCardOnPage);
+    placesList.append(cardElement);
 })
 
